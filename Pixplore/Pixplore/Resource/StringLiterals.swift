029a50003b8792adb3c05b21c90ae3998ef715fc
Minsoo Kang <alstn38@naver.com>
2025-01-17 19:49:17 +0900

    chore: StringLiterals 검색 추가

---
+++ Pixplore/Pixplore/Resource/StringLiterals.swift
@@ -0,0 +1,16 @@
+//
+//  StringLiterals.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/17/25.
+//
+
+import Foundation
+
+enum StringLiterals {
+    
+    enum Search {
+        static let title: String = "사진 검색하기"
+        static let searchBarPlaceholder: String = "키워드 검색"
+    }
+}
