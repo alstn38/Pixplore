7994515fbbf896e25d974cfb733097032473f1e5
Minsoo Kang <alstn38@naver.com>
2025-01-17 19:50:51 +0900

    feat: UICollectionViewCell Identifier 추가

---
+++ Pixplore/Pixplore/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/17/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
