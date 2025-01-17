7c861457d7361073decb4ad94dc366644ffa3370
Minsoo Kang <alstn38@naver.com>
2025-01-17 21:10:53 +0900

    feat: SearchView ColorFilter CollectionView Delegate, DataSource 설정

--- Pixplore/Pixplore/Present/Search/ViewController/SearchViewController.swift
+++ Pixplore/Pixplore/Present/Search/ViewController/SearchViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class SearchViewController: UIViewController {
     
     private let searchView = SearchView()
+    private let colorFilterArray: [ColorFilter] = ColorFilter.allCases
     private let searchController: UISearchController = {
         let searchController = UISearchController()
         searchController.searchBar.placeholder = StringLiterals.Search.searchBarPlaceholder
@@ -24,10 +25,59 @@ final class SearchViewController: UIViewController {
         super.viewDidLoad()
         
         configureNavigation()
+        configureDelegate()
     }
     
     private func configureNavigation() {
         navigationItem.title = StringLiterals.Search.title
         navigationItem.searchController = searchController
+    }
+    
+    private func configureDelegate() {
+        searchController.delegate = self
+        
+        searchView.colorCollectionView.delegate = self
+        searchView.colorCollectionView.dataSource = self
+        searchView.colorCollectionView.register(
+            ColorCollectionViewCell.self,
+            forCellWithReuseIdentifier: ColorCollectionViewCell.identifier
+        )
+        
+        searchView.pictureCollectionView.delegate = self
+        searchView.pictureCollectionView.dataSource = self
+    }
+}
+
+// MARK: - UISearchControllerDelegate
+extension SearchViewController: UISearchControllerDelegate {
+    
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch collectionView {
+        case searchView.colorCollectionView:
+            return colorFilterArray.count
+        case searchView.pictureCollectionView:
+            return 0
+        default:
+            return 0
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch collectionView {
+        case searchView.colorCollectionView:
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: ColorCollectionViewCell.identifier,
+                for: indexPath
+            ) as? ColorCollectionViewCell else { return UICollectionViewCell() }
+            cell.configureCell(colorFilterArray[indexPath.row])
+            return cell
+        default:
+            return UICollectionViewCell()
+        }
     }
 }
