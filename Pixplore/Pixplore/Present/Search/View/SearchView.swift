82fb096ef0be2aaac669e91cc186b2eeffbf0379
Minsoo Kang <alstn38@naver.com>
2025-01-17 19:50:01 +0900

    feat: SearchView View 속성 정의 및 Layout 설정

---
+++ Pixplore/Pixplore/Present/Search/View/SearchView.swift
@@ -0,0 +1,72 @@
+//
+//  SearchView.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/17/25.
+//
+
+import SnapKit
+import UIKit
+
+final class SearchView: UIView {
+    
+    let colorCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let insetSpacing: CGFloat = 10
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = insetSpacing
+        layout.sectionInset = UIEdgeInsets(top: insetSpacing, left: insetSpacing, bottom: insetSpacing, right: insetSpacing)
+        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    
+    let pictureCollectionView: UICollectionView = {
+        let screenWidth: CGFloat = UIScreen.main.bounds.width
+        let insetSpacing: CGFloat = 5
+        let cellLength: CGFloat = (screenWidth - insetSpacing)/2
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = insetSpacing
+        layout.minimumLineSpacing = insetSpacing
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        layout.itemSize = CGSize(width: cellLength, height: cellLength / 3 * 4)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureView() {
+        backgroundColor = .white
+    }
+    
+    private func configureHierarchy() {
+        addSubviews(
+            colorCollectionView,
+            pictureCollectionView
+        )
+    }
+    
+    private func configureLayout() {
+        colorCollectionView.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(safeAreaLayoutGuide)
+            $0.height.equalTo(34)
+        }
+        
+        pictureCollectionView.snp.makeConstraints {
+            $0.top.equalTo(colorCollectionView.snp.bottom)
+            $0.horizontalEdges.bottom.equalTo(safeAreaLayoutGuide)
+        }
+    }
+}
