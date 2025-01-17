d68d7dadd42059a664d5a739baed2ebd6cce87a6
Minsoo Kang <alstn38@naver.com>
2025-01-17 16:42:48 +0900

    init: Pixplore project 생성

---
+++ Pixplore/Pixplore/App/SceneDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  SceneDelegate.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/17/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let pixploreTabBarController = PixploreTabBarController()
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = pixploreTabBarController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
---
+++ Pixplore/Pixplore/Present/TabBar/PixploreTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  PixploreTabBarController.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/17/25.
+//
+
+import UIKit
+
+final class PixploreTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
