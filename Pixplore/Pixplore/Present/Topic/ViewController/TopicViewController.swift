1d1bf976a5c6217534df7102fc40474f2b7b04e4
Minsoo Kang <alstn38@naver.com>
2025-01-17 19:49:36 +0900

    feat: TopicViewController 생성

---
+++ Pixplore/Pixplore/Present/Topic/ViewController/TopicViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TopicViewController.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/17/25.
+//
+
+import UIKit
+
+final class TopicViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
