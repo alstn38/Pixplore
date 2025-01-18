e19b9453dc450590e47d5144aedc39af6a3af4b7
Minsoo Kang <alstn38@naver.com>
2025-01-18 23:01:03 +0900

    feat: NetworkResult 정의

---
+++ Pixplore/Pixplore/Network/NetworkBase/NetworkResult.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkResult.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/18/25.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case badRequest
+    case unauthorized
+    case forbidden
+    case notFound
+    case severError
+    case unknown
+    
+    var description: String {
+        switch self {
+        case .invalidURL:
+            return "Failed to create url"
+        case .badRequest:
+            return "The request was unacceptable, often due to missing a required parameter"
+        case .unauthorized:
+            return "Invalid Access Token"
+        case .forbidden:
+            return "Missing permissions to perform request"
+        case .notFound:
+            return "The requested resource doesn’t exist"
+        case .severError:
+            return "Something went wrong on our end"
+        case .unknown:
+            return "Unknown Error"
+        }
+    }
+}
