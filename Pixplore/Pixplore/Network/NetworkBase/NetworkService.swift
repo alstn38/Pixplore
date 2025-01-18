bd6a66d48cd7227a75db176e0ed279323404595f
Minsoo Kang <alstn38@naver.com>
2025-01-18 23:01:16 +0900

    feat: NetworkService 정의

---
+++ Pixplore/Pixplore/Network/NetworkBase/NetworkService.swift
@@ -0,0 +1,57 @@
+//
+//  NetworkService.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/18/25.
+//
+
+import Alamofire
+
+final class NetworkService {
+    
+    static let shared = NetworkService()
+    
+    private init() { }
+    
+    func request<T: Decodable>(
+        endPoint: EndPointProtocol,
+        responseType: T.Type,
+        completionHandler: @escaping (Result<T, NetworkError>) -> Void
+    ) {
+        guard let url = endPoint.url else {
+            return completionHandler(.failure(.invalidURL))
+        }
+        
+        AF.request(
+            url,
+            method: endPoint.httpMethod,
+            parameters: endPoint.params,
+            headers: HTTPHeaders(endPoint.headers ?? [:])
+        )
+        .validate(statusCode: 200...299)
+        .responseDecodable(of: T.self) { [weak self] response in
+            guard let self else { return }
+            switch response.result {
+            case .success(let value):
+                completionHandler(.success(value))
+            case .failure(let error):
+                print(response.response?.statusCode)
+                print(error.localizedDescription)
+                let errorType = self.getNetworkError(statusCode: response.response?.statusCode)
+                completionHandler(.failure(errorType))
+            }
+        }
+    }
+    
+    private func getNetworkError(statusCode: Int?) -> NetworkError {
+        switch statusCode {
+        case 400: return .badRequest
+        case 401: return .unauthorized
+        case 403: return .forbidden
+        case 404: return.notFound
+        case 500: return .severError
+        case 503: return .severError
+        default: return .unknown
+        }
+    }
+}
