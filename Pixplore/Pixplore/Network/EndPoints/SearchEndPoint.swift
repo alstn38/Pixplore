691e6afdd3b4e04c8b0804425347f82f6e0b8357
Minsoo Kang <alstn38@naver.com>
2025-01-18 23:01:44 +0900

    feat: SerachEndPoint 정의

---
+++ Pixplore/Pixplore/Network/EndPoints/SearchEndPoint.swift
@@ -0,0 +1,61 @@
+//
+//  SearchEndPoint.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/18/25.
+//
+
+import Alamofire
+import Foundation
+
+enum SearchEndPoint: EndPointProtocol {
+    case searchPicture(
+        query: String,
+        page: Int,
+        perPage: Int = 20,
+        orderBy: String,
+        color: String
+    )
+}
+
+extension SearchEndPoint {
+    
+    var url: URL? {
+        return URL(string: baseURL + path)
+    }
+    
+    var baseURL: String {
+        return Secret.baseURL
+    }
+    
+    var path: String {
+        switch self {
+        case .searchPicture:
+            return "/search/photos"
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .searchPicture:
+            return .get
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Authorization": Secret.clientID]
+    }
+    
+    var params: Parameters? {
+        switch self {
+        case .searchPicture(let query, let page, let perPage, let orderBy, let color):
+            return [
+                "query": query,
+                "page": page,
+                "per_page": perPage,
+                "order_by": orderBy,
+                "color": color
+            ]
+        }
+    }
+}
