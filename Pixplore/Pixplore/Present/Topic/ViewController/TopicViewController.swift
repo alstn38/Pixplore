f903ff5856bed0b8316f3239d970032c433ccc58
Minsoo Kang <alstn38@naver.com>
2025-01-18 15:45:52 +0900

    feat: TopicViewController CollectionView Delegate, Datasource 설정

--- Pixplore/Pixplore/Present/Topic/ViewController/TopicViewController.swift
+++ Pixplore/Pixplore/Present/Topic/ViewController/TopicViewController.swift
@@ -8,10 +8,73 @@
 import UIKit
 
 final class TopicViewController: UIViewController {
+    
+    private let topicView = TopicView()
+    private var topicPictureArray: [TopicView.TopicSection] = [
+        .goldenHour([dummyTopicPictures, dummyTopicPictures, dummyTopicPictures]),
+        .business([dummyTopicPictures, dummyTopicPictures, dummyTopicPictures, dummyTopicPictures, dummyTopicPictures, dummyTopicPictures]),
+        .architecture([dummyTopicPictures, dummyTopicPictures, dummyTopicPictures, dummyTopicPictures, dummyTopicPictures])
+    ]
+    
+    override func loadView() {
+        view = topicView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        configureNavigation()
+        configureDelegate()
+    }
+    
+    private func configureNavigation() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = StringLiterals.Topic.title
+    }
+    
+    private func configureDelegate() {
+        topicView.topicPictureCollectionView.delegate = self
+        topicView.topicPictureCollectionView.dataSource = self
+        topicView.topicPictureCollectionView.register(
+            TopicPictureCollectionViewCell.self,
+            forCellWithReuseIdentifier: TopicPictureCollectionViewCell.identifier
+        )
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+extension TopicViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return topicPictureArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        switch topicPictureArray[section] {
+        case let .goldenHour(items):
+            return items.count
+        case let .business(items):
+            return items.count
+        case let .architecture(items):
+            return items.count
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: TopicPictureCollectionViewCell.identifier,
+            for: indexPath
+        ) as? TopicPictureCollectionViewCell else { return UICollectionViewCell() }
+        
+        switch topicPictureArray[indexPath.section] {
+        case let .goldenHour(items):
+            print("")
+        case let .business(items):
+            print("")
+        case let .architecture(items):
+            print("")
+        }
+        
+        return cell
     }
 }
