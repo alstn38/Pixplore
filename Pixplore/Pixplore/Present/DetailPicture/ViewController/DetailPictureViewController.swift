fb56e075d0ebe71f18a5992e84cc25724acacc1d
Minsoo Kang <alstn38@naver.com>
2025-01-19 19:31:19 +0900

    feat: DetailPicture ViewController 네트워크 통신 구현

--- Pixplore/Pixplore/Present/DetailPicture/ViewController/DetailPictureViewController.swift
+++ Pixplore/Pixplore/Present/DetailPicture/ViewController/DetailPictureViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 final class DetailPictureViewController: UIViewController {
     
     private let detailPictureView = DetailPictureView()
+    private let picture: Picture
+    
+    init(picture: Picture) {
+        self.picture = picture
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         view = detailPictureView
@@ -17,5 +28,22 @@ final class DetailPictureViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        detailPictureView.configureView(picture: picture)
+        getDetailPicture(imageID: picture.id)
+    }
+    
+    private func getDetailPicture(imageID: String) {
+        let endPoint = DetailPictureEndPoint.detailPicture(imageID: imageID)
+        
+        NetworkService.shared.request(endPoint: endPoint, responseType: DetailPicture.self) { [weak self] response in
+            guard let self else { return }
+            switch response {
+            case .success(let value):
+                detailPictureView.configureView(detailPicture: value)
+            case .failure(let error):
+                self.presentWarningAlert(message: error.description)
+            }
+        }
     }
 }
