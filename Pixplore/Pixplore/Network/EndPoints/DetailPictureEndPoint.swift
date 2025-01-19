b3bdc652b72379b0defb30a94bba8b59500669bf
Minsoo Kang <alstn38@naver.com>
2025-01-19 19:30:42 +0900

    feat: DetailPictureEndPoint 정의

---
+++ Pixplore/Pixplore/Network/EndPoints/DetailPictureEndPoint.swift
@@ -0,0 +1,49 @@
+//
+//  DetailPictureEndPoint.swift
+//  Pixplore
+//
+//  Created by 강민수 on 1/19/25.
+//
+
+import Alamofire
+import Foundation
+
+enum DetailPictureEndPoint: EndPointProtocol {
+    case detailPicture(imageID: String)
+}
+
+extension DetailPictureEndPoint {
+    
+    var url: URL? {
+        return URL(string: baseURL + path)
+    }
+    
+    var baseURL: String {
+        return Secret.baseURL
+    }
+    
+    var path: String {
+        switch self {
+        case .detailPicture(let imageID):
+            return "/photos/\(imageID)/statistics"
+        }
+    }
+    
+    var httpMethod: HTTPMethod {
+        switch self {
+        case .detailPicture:
+            return .get
+        }
+    }
+    
+    var headers: HTTPHeaders? {
+        return ["Authorization": Secret.clientID]
+    }
+    
+    var parameters: Parameters? {
+        switch self {
+        case .detailPicture:
+            return nil
+        }
+    }
+}
