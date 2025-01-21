d9bfcb7554e697a085a242b1680bdbe21ea42823
Minsoo Kang <alstn38@naver.com>
2025-01-21 18:28:26 +0900

    feat: Shorts ImageView 로딩 기능 추가

--- Pixplore/Pixplore/Present/Shorts/Cell/ShortsCollectionViewCell.swift
+++ Pixplore/Pixplore/Present/Shorts/Cell/ShortsCollectionViewCell.swift
@@ -11,6 +11,12 @@ import UIKit
 
 final class ShortsCollectionViewCell: UICollectionViewCell {
     
+    private let activityIndicatorView: UIActivityIndicatorView = {
+        let activityIndicatorView = UIActivityIndicatorView(style: .medium)
+        activityIndicatorView.hidesWhenStopped = true
+        return activityIndicatorView
+    }()
+    
     private let shortsImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -54,14 +60,19 @@ final class ShortsCollectionViewCell: UICollectionViewCell {
     }
     
     func configureCell(_ picture: Picture) {
+        activityIndicatorView.startAnimating()
         userNameLabel.text = picture.user.name
         pictureDateLabel.text = picture.createdAt
         
         let userProfileURL = URL(string: picture.user.profileImage.mediumSizeLink)
         userProfileImageView.kf.setImage(with: userProfileURL)
         
+        let screenWidth = UIScreen.main.bounds.width
         let shortsImageURL = URL(string: picture.urls.originalLink)
-        shortsImageView.kf.setImage(with: shortsImageURL) // TODO: 이미지 resize
+        shortsImageView.kf.setImage(with: shortsImageURL) { [weak self] _ in
+            self?.shortsImageView.image = self?.shortsImageView.image?.resize(newWidth: screenWidth)
+            self?.activityIndicatorView.stopAnimating()
+        }
     }
     
     private func configureHierarchy() {
@@ -69,7 +80,8 @@ final class ShortsCollectionViewCell: UICollectionViewCell {
             shortsImageView,
             userProfileImageView,
             userNameLabel,
-            pictureDateLabel
+            pictureDateLabel,
+            activityIndicatorView
         )
     }
     
@@ -91,6 +103,10 @@ final class ShortsCollectionViewCell: UICollectionViewCell {
         pictureDateLabel.snp.makeConstraints {
             $0.top.equalTo(userNameLabel.snp.bottom).offset(3)
             $0.leading.equalTo(userProfileImageView.snp.trailing).offset(15)
+        }
+        
+        activityIndicatorView.snp.makeConstraints {
+            $0.center.equalToSuperview()
         }
     }
 }
