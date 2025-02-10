f9507945974b1a4564de57f372f7bcd1011b2516
Minsoo Kang <alstn38@naver.com>
2025-02-10 19:03:05 +0900

    refactor: DetailPictureViewController MVVM 구조 변경

--- Pixplore/Pixplore/Present/DetailPicture/ViewController/DetailPictureViewController.swift
+++ Pixplore/Pixplore/Present/DetailPicture/ViewController/DetailPictureViewController.swift
@@ -9,12 +9,16 @@ import UIKit
 
 final class DetailPictureViewController: UIViewController {
     
+    private let viewModel: DetailPictureViewModel
+    private let input: DetailPictureViewModel.Input
     private let detailPictureView = DetailPictureView()
-    private var detailPicture: DetailPicture?
-    private let picture: Picture
     
-    init(picture: Picture) {
-        self.picture = picture
+    init(viewModel: DetailPictureViewModel) {
+        self.viewModel = viewModel
+        self.input = DetailPictureViewModel.Input(
+            viewDidLoad: CurrentValueRelay(()),
+            chartSegmentedControlDidChange: CurrentValueRelay(.view)
+        )
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -30,32 +34,48 @@ final class DetailPictureViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureBindData()
         configureAddTarget()
-        detailPictureView.configureView(picture: picture)
-        getDetailPicture(imageID: picture.id)
+        input.viewDidLoad.send(())
     }
     
-    private func configureAddTarget() {
-        detailPictureView.chartSegmentedControl.addTarget(self, action: #selector(chartSegmentedControlDidChange), for: .valueChanged)
-    }
-    
-    private func getDetailPicture(imageID: String) {
-        let endPoint = PictureEndPoint.detailPicture(imageID: imageID)
+    private func configureBindData() {
+        let output = viewModel.transform(from: input)
         
-        NetworkService.shared.request(endPoint: endPoint, responseType: DetailPicture.self) { [weak self] response in
+        output.pictureData.bind { [weak self] picture in
             guard let self else { return }
-            switch response {
-            case .success(let value):
-                self.detailPicture = value
-                detailPictureView.configureView(detailPicture: value)
-            case .failure(let error):
-                self.presentWarningAlert(message: error.description)
-            }
+            detailPictureView.configureView(picture: picture)
+        }
+        
+        output.detailPicture.bind { [weak self] detailPicture in
+            guard
+                let self,
+                let detailPicture
+            else { return }
+            detailPictureView.configureView(detailPicture: detailPicture)
+        }
+        
+        output.detailPictureChartDataArray.bind { [weak self] chartDataEntry in
+            guard let self else { return }
+            detailPictureView.configureLickChart(chartDataEntry)
+        }
+        
+        output.presentError.bind { [weak self] errorDescription in
+            guard let self else { return }
+            presentWarningAlert(message: errorDescription)
         }
     }
     
+    private func configureAddTarget() {
+        detailPictureView.chartSegmentedControl.addTarget(
+            self,
+            action: #selector(chartSegmentedControlDidChange),
+            for: .valueChanged
+        )
+    }
+    
     @objc private func chartSegmentedControlDidChange(_ sender: UISegmentedControl) {
-        guard let detailPicture else { return }
-        detailPictureView.configureView(detailPicture: detailPicture)
+        let chartPresentType = DetailPictureViewModel.chartPresentType(rawValue: sender.selectedSegmentIndex) ?? .view
+        input.chartSegmentedControlDidChange.send(chartPresentType)
     }
 }
